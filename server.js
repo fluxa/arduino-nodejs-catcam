9c623031f92200f1153e155f376e7a87726d0c47
Juan-Manuel Fluxà <jmfluxa@gmail.com>
2013-03-15 01:26:18 +0800

    added presets! redis backed

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var serialPort = require("serialport");
 var SerialPort = serialPort.SerialPort;
+var redis = require("redis"),
+    client = redis.createClient();
 
 String.prototype.format = function() {
   var args = arguments;
@@ -16,6 +18,25 @@ String.prototype.format = function() {
   });
 };
 
+// Redis
+var presetDefault = [[90,90],[90,90],[90,90],[90,90]]; //default value
+var presetsHash = "presetsHash";
+var presetsData = null;
+client.on("error", function(err){
+  console.log(err);
+});
+client.get(presetsHash,function(err,reply){
+  if(err){
+    console.log("error");
+  } else {
+    try{
+      presetsData = JSON.parse(reply);
+    } catch(e) {
+      presetsData = JSON.stringify(presetDefault);
+    }
+    console.log("redis data: " + presetsData);
+  }
+});
 
 var info = {
 	name:'arduinode',
@@ -48,6 +69,8 @@ server.listen(port, function() {
   console.log(info.name + " app deployed and listening on port " + port);
 });
 
+
+
 // Sockets
 io.set('log level',1); //reduce logging
 
@@ -59,17 +82,23 @@ io.sockets.on('connection', function (socket) {
     sp.write(data+'\n');
   });
 
+  socket.on('onRedisData', function(data){
+    if(client) {
+      console.log('got: ' + data);
+      client.set(presetsHash,JSON.stringify(data),redis.print);
+    }
+  });
   
   socket.on('disconnect', function (data) {
-    
-    
-    
   });
 
   // start
   socket.emit('onConnect',{socketid:socket.id});
+  socket.emit('redisData',{data:presetsData});
 
 });
+
+
 
 // Serial
 console.log('connecting to serial port %s',spport);
@@ -88,3 +117,5 @@ serialPort.list(function (err, ports) {
     console.log(port.manufacturer);
   });
 });
+
+
